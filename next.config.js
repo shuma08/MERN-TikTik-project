0f3b093364190bcefe04174709831f0771b8d039
vitaliy <vitaliy.shumskyy@orb-pay.com>
2022-10-24 19:26:35 +0300

    gooogle auth + logout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['drive.google.com', 's.dou.ua',"lh3.googleusercontent.com"]
+  },
   swcMinify: true,
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,46 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
 
-const Home: NextPage = () => {
+import Head from 'next/head';
+import axios from 'axios';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
+
+
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({ videos }: IProps) => {
+
+  console.log(videos);
+
   return (
     <>
       <Head>
         <title>TikTik MERN-project</title>
       </Head>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+      <div className='flex flex-col gap-10 videos h-full'>
+        {videos.length ? (
+          videos.map((video: Video) => (
+            <VideoCard post={video} key={video._id} />
+          ))
+        ) : (
+          <NoResults text={"No Videos"} />
+        )
+        }
       </div>
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get('http://localhost:3000/api/post');
+
+  return {
+    props: {
+      videos: data
+    }
+  }
 }
 
 export default Home
